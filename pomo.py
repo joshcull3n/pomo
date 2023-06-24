d0777c7d3cb41115d44c3e070dcffb6cc400d2a3
joshcull3n <8888780+joshcull3n@users.noreply.github.com>
2023-06-23 22:05:10 -0700

    make it better

--- pomo.py
+++ pomo.py
@@ -1,48 +1,85 @@
 import time
 import os
-from plyer import notification
+#from plyer import notification
 
 pomo = True
-count = 0
+plural = 'pomo'
+
 timer = 2700  # 45 minutes
 breakTime = 900  # 15 minutes
-pomoCount = 0
+
+class bcolors:
+  HEADER = '\033[95m'
+  OKBLUE = '\033[94m'
+  OKCYAN = '\033[96m'
+  OKGREEN = '\033[92m'
+  WARNING = '\033[93m'
+  FAIL = '\033[91m'
+  END = '\033[0m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+  BOLDRED = '\x1b[1;31m'
+
+def pomoCompleteString(num):
+  os.system('clear')
+  pomoSymbol = "🍅 "
+  if (num == 0):
+    symbols = '0'
+  else:
+    symbols = num*pomoSymbol
+  pomoString = "pomos completed : " + symbols + '\n'
+  print(pomoString)
 
 if __name__ == "__main__":
+  pomoCount = 0
+  breakCount = 0
+  
   while (pomo):
     # work
     count = 0
+    if pomoCount > 1:
+      plural = 'pomos'
     while count < timer:
-      os.system('clear')
-      print("The pomodoro timer has started, start working!")
+      pomoCompleteString(pomoCount)
+      print("the pomodoro timer has started, start working!")
       count += 1
       print(str(count), '/', str(timer))
+      print()
       time.sleep(1)
     pomoCount += 1
-    notification.notify(
-      title="Good work!",
-      message="Take a 15 minute break! You have completed " +
-      str(pomoCount) + " pomodoros so far",
-    )
-    # os.system('clear')
+    #notification.notify(
+    #  title="good work!",
+    #  message="take a 15 minute break! you have completed " +
+    #  str(pomoCount) + " pomodoros so far",
+    #)
+    
+    pomoCompleteString(pomoCount)
     print('pomo finished.')
-    input('press any key to start your break...')
+    continuePomo = input('enter \'q\' to end the session.\npress any other key to start your break...\n\n')
+    if continuePomo.lower() == 'q':
+      pomo = False
+      break
 
     # break
     count = 0
     while count < breakTime:
-      os.system('clear')
-      print("Your break has started, enjoy.")
+      pomoCompleteString(pomoCount)
+      print("your break has started, enjoy.")
       count += 1
       print(str(count), ' / ', str(breakTime))
+      print()
       time.sleep(1)
-    notification.notify(
-      title="Back to work!",
-      message="Try doing another pomodoro...",
-    )
-    print('break finished.')
-    continuePomo = input('start another pomo? (Y/n) : ')
-    if continuePomo == 'n':
+    breakCount += 1
+    #notification.notify(
+    #  title="Back to work!",
+    #  message="Try doing another pomodoro...",
+    #)
+    pomoCompleteString(pomoCount)
+    print('break finished.\n')
+    continuePomo = input('enter \'q\' to end the session.\npress any other key to start the next pomo...\n\n')
+    if continuePomo == 'q':
       pomo = False
-  os.system('clear')
-  print('you completed', str(pomoCount), 'pomo(s).')
+  pomoCompleteString(pomoCount)
+  print('you completed' + bcolors.BOLDRED, str(pomoCount), plural + bcolors.END + '!')
+  print('that\'s ' + str(int((pomoCount*timer)/60)) + ' minutes of productivity and ' + str(int((breakCount*breakTime)/60)) + ' minutes of breaktime.\n')
+
